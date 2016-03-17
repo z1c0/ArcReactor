eca44d3439f11eaaa458a7c8761a0ba81b6d92c9
unknown <wolfgangz@lnz-wolfgangz2.microfocus.com>
2016-03-17 16:38:25 +0100

    Added helper class ColorPulser. Improved animations

--- ArcReactor.ino
+++ ArcReactor.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include "ColorPulser.h"
 
 #define PIN 6
 #define LEDS 12
@@ -6,83 +7,124 @@
 #define CORE_LED_1 10
 #define CORE_LED_2 11
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
 #define CYAN_R 58
 #define CYAN_G 209
 #define CYAN_B 228
-uint32_t cyan = strip.Color(CYAN_R, CYAN_B, CYAN_G);
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
 uint32_t black = strip.Color(0, 0, 0);
+uint32_t cyan = strip.Color(CYAN_R, CYAN_B, CYAN_G);
+uint32_t halfWhite = strip.Color(150, 255, 255);
 uint32_t white = strip.Color(255, 255, 255);
 
 
+void core(uint32_t color)
+{
+  strip.setPixelColor(CORE_LED_1, color);
+  strip.setPixelColor(CORE_LED_2, color);
+  strip.show();
+}
+
+void ring(uint32_t color, uint32_t wait = 0)
+{
+  for (int16_t i = 0; i < RING_LEDS; i++)
+  {
+    strip.setPixelColor(i, color);
+    if (wait != 0)
+    {
+      strip.show();
+      delay(wait);
+    }
+  } 
+  strip.show();
+}
+
+void corePulse()
+{
+  const uint32_t WAIT = 15; 
+  int16_t count = 0;
+
+  ring(cyan);
+  
+  ColorPulser pulser(Color(CYAN_R, CYAN_G, CYAN_B), Color(255, 255, 255));  
+  while (count < 10)
+  { 
+    auto& c = pulser.Value();
+    core(strip.Color(c.R, c.G, c.B));
+    delay(WAIT);
+    if (!pulser.Animate())
+    {
+      count++;
+    }
+  }
+}
+
+void ringPulse()
+{
+  const uint32_t WAIT = 15; 
+  int16_t count = 0;
+
+  core(cyan);
+  
+  ColorPulser pulser(Color(CYAN_R, CYAN_G, CYAN_B), Color(255, 255, 255));  
+  while (count < 10)
+  { 
+    auto& c = pulser.Value();
+    ring(strip.Color(c.R, c.G, c.B));
+    delay(WAIT);
+    if (!pulser.Animate())
+    {
+      count++;
+    }
+  }
+}
+
+void ringWander()
+{
+  int16_t wanderPos = 0;
+  
+  for (int32_t j = 0; j < RING_LEDS * 10; j++)
+  {  
+    for (int16_t i = 0; i < RING_LEDS; i++)
+    {    
+      strip.setPixelColor(i, cyan);
+    }
+    strip.setPixelColor((wanderPos - 1) % RING_LEDS, halfWhite);
+    strip.setPixelColor(wanderPos, white);
+    strip.setPixelColor((wanderPos + 1) % RING_LEDS, halfWhite);
+    strip.show();
+
+    delay(500);
+
+    wanderPos = (wanderPos + 1) % RING_LEDS; 
+  }
+}
+
 void startUp()
 {
-  strip.setPixelColor(CORE_LED_1, cyan);
-  strip.setPixelColor(CORE_LED_2, cyan);
-  strip.show();
+  core(cyan);
 
   for (int16_t j = 0; j < 3; j++)
   {  
     delay(1000);
     
     const uint32_t wait = 25;
-    for (int16_t i = 0; i < RING_LEDS; i++)
-    {
-      strip.setPixelColor(i, cyan);
-      strip.show();
-      delay(wait);
-    }  
     
+    ring(cyan, wait);
     delay(1000);
     
-    for (int16_t i = 0; i < RING_LEDS; i++)
-    {
-      strip.setPixelColor(i, black);
-      strip.show();
-      delay(wait);
-    }
-  }
-}
-
-void corePulse()
-{
-  const uint32_t WAIT = 15;  
-  uint8_t r = CYAN_R;
-  uint8_t g = CYAN_G;
-  uint8_t b = CYAN_B;
-
-  while (r != 255 || g != 255 || b != 255)
-  { 
-    auto c = strip.Color(r, g, b);
-    strip.setPixelColor(CORE_LED_1, c);
-    strip.setPixelColor(CORE_LED_2, c);
-    strip.show();
-    delay(WAIT);
-    r = min(r + 1, 255);
-    g = min(g + 1, 255);
-    b = min(b + 1, 255);        
-  }
-  
-  while (r != CYAN_R || g != CYAN_G || b != CYAN_B)
-  { 
-    auto c = strip.Color(r, g, b);
-    strip.setPixelColor(CORE_LED_1, c);
-    strip.setPixelColor(CORE_LED_2, c);
-    strip.show();
-    delay(WAIT);
-    r = max(r - 1, CYAN_R);
-    g = max(g - 1, CYAN_G);
-    b = max(b - 1, CYAN_B);    
+    ring(black, wait);
   }
 }
 
 void loop()
 {
-  //corePulse();
+  corePulse();
 
-  // ringpulse
+  ringPulse();
 
-  // ringwander
+  ringWander();
 }
 
 void setup()
